6a33c847cd386f3cd1bb4b9a92be79501f535343
Thiện Nhân <nhanntn2203@gmail.com>
2025-04-13 00:02:51 +0700

    fix css js tailwind package and vite

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/views/**/*.blade.php",
+    "./resources/js/**/*.js",
+    "./resources/css/**/*.css",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite'
+
 
 export default defineConfig({
     plugins: [
+        tailwindcss(), // Tailwind CSS plugin
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js'], // Input files for Vite
             refresh: true,
         }),
     ],
+    css: {
+        postcss: {
+            plugins: [tailwindcss] // Ensure Tailwind is properly processed
+        }
+    }
 });
+
